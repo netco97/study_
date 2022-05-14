86fc13575f88a75d9324a2758fee24e4c1dc5a6b
netco97 <101931428+netco97@users.noreply.github.com>
2022-05-14 22:08:26 +0900

    Add files via upload

--- src/programers_test/Main.java
+++ src/programers_test/Main.java
@@ -7,29 +7,15 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		Scanner Sc = new Scanner(System.in);
-		String s;
-		s=Sc.nextLine();
-		String str[] = s.split("");
-		int index=0;
-		
-		for(int i=0;i<str.length;i++)
-		{
-			if(str[i].equals(" "))
+		int N=Sc.nextInt();
+
+		int count = 0;
+		for (int i = 1; i <= N; i++) {
+			if (N % i == 0)
 			{
-				index=0;
+				count+=i;
 			}
-			else if(index%2==0)
-			{
-				str[i]=str[i].toUpperCase();
-				index++;
-			}
-			else if(index%2!=0)
-			{
-				str[i]=str[i].toLowerCase();
-				index++;
-			}
-			System.out.print(str[i]);
+		}
+		System.out.println(count);
 		}
 	}
-
-}
