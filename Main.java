b5bb61c249882343a86c1142c1b2948af654c5bb
netco97 <101931428+netco97@users.noreply.github.com>
2022-06-28 21:58:12 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
-package test_1;
+package programers_test;
+
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) 
 	{
-		Solution So = new Solution();
+		Scanner Sc = new Scanner(System.in);
+		String s;
+		s=Sc.next();
+		System.out.println(Integer.parseInt(s));
 	}
-
+	
+	
 }
