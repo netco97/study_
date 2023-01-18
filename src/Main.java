4882feb9192c2896963ad498dcd4f4f4f694864b
Sangmin <101931428+netco97@users.noreply.github.com>
2023-01-18 21:50:42 +0900

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        //Map 정리
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1,"A");
+        map.put(2,"B");
+        map.put(3,"C");
+
+        //1
+        for(Integer key : map.keySet()){
+            System.out.println("key: "+key+" values: "+map.get(key));
+        }
+
+        //2 entrySet
+        for(Map.Entry<Integer, String> elem : map.entrySet()){
+            System.out.println("key: "+elem.getKey()+" values: "+elem.getValue());
+        }
+    }
+
+}
